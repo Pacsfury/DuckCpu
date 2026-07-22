54012d92b0c36b39ce8d44d311fb092444941bbf
Pacsfury <280690649+Pacsfury@users.noreply.github.com>
2026-07-22 11:36:39 +0200

    Refactor register and port initialization in main.rs

--- main.rs
+++ main.rs
@@ -1,19 +1,18 @@
 fn main() {
     let mut ram: [u8; 4096] = [0; 4096];
-    let mut reg: [i32; 4] = [0b00, 0b00, 0b00, 0b00];
-    let mut ports: [i32; 16] = [0b00; 256];
+    let mut reg: [i32; 4] = [0, 0, 0, 0];
+    let mut ports: [i32; 16] = [0; 16];
 
-    execute_line("0000101100000101".to_string(), &mut ram, &mut reg, &mut ports);
-    execute_line("0000000100000001".to_string(), &mut ram, &mut reg, &mut ports);
-    execute_line("0000001100100000".to_string(), &mut ram, &mut reg, &mut ports);
-    execute_line("0000110000010010".to_string(), &mut ram, &mut reg, &mut ports);
+    execute_line("0000101100000101", &mut ram, &mut reg, &mut ports);
+    execute_line("0000000100000001", &mut ram, &mut reg, &mut ports);
+    execute_line("0000001100100000", &mut ram, &mut reg, &mut ports);
+    execute_line("0000110000010010", &mut ram, &mut reg, &mut ports);
 
     println!("Registers: {:?}", reg);
     println!("Port 1: {}", ports[1]);
 }
 
-
-fn execute_line(line: String, _ram: &mut [u8; 4096], reg: &mut [i32; 4], ports: &mut [i32; 16]) {
+fn execute_line(line: &str, _ram: &mut [u8; 4096], reg: &mut [i32; 4], ports: &mut [i32; 16]) {
     let (operation, operand) = line.split_at(8);
     let (op1, op2) = operand.split_at(4);
 
